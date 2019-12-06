6d29273d0f5f6e633551c212d5defe8631accde4
Marcin Sendyka <marcin.sendyka@ocado.com>
2019-12-06 09:59:04 +0100

    2019 p6

--- 2019/src/main/java/com/msendyka/adventofcode/p6/P6.java
+++ 2019/src/main/java/com/msendyka/adventofcode/p6/P6.java
@@ -1,6 +1,7 @@
 package com.msendyka.adventofcode.p6;
 
 import com.msendyka.adventofcode.Functions;
+
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.collection.Tree;
@@ -9,23 +10,39 @@ public class P6 {
 
     public static void main(String[] args) {
         partOne();
-
-        List<String> strings = Functions.readInput("p6/test.txt");
+        List<String> strings = Functions.readInput("p6/input.txt");
         List<Tuple2> tuples = strings.map(s -> s.split("\\)"))
                 .map(arr -> new Tuple2(arr[0], arr[1]));
-//        Tuple2 root = tuples.find(a -> a._2.equals("YOU")).get();
-//        tuples = tuples.push(new Tuple2( "YOU", null ));
         tuples = tuples.append(new Tuple2(null, "COM"));
 
         List<Tree.Node<Tuple2>> treeList = Tree.build(tuples, tuple2 -> tuple2._2, tuple2 -> tuple2._1);
 
-        System.out.println(treeList);
         Tree.Node<Tuple2> tree = treeList.head();
         List<Tree.Node<Tuple2>> nodes = tree.traverse(Tree.Order.IN_ORDER)
                 .filter(node -> findBoth(node)).toList();
 
-        nodes.forEach(a -> System.out.println(a.size()));
-//        System.out.println(nodes.map(list -> list.size()));
+        Tree.Node<Tuple2> x = nodes.minBy(a -> a.size()).get();
+
+        int you = find(x, "YOU");
+        int san = find(x, "SAN");
+        System.out.println(you + san);
+    }
+
+    private static int find(Tree.Node<Tuple2> a, String nodeVal) {
+        if (a == null) {
+            return -1;
+        }
+        int dist = -1;
+        if (a.get().equals(nodeVal)) {
+            return dist + 1;
+        }
+        for (Tree.Node<Tuple2> tuple2s : a.getChildren()) {
+            if (tuple2s.traverse().find(node -> node.getValue()._2().equals(nodeVal)).isDefined()) {
+                return find(tuple2s, nodeVal) + 1;
+
+            }
+        }
+        return dist;
     }
 
     private static boolean findBoth(Tree.Node<Tuple2> node) {
