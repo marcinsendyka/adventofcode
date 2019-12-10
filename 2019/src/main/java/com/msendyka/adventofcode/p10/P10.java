878694cb5d460ea0e92c3e4366d885013fdf228c
smajlo <marcin.sendyka@gmail.com>
2019-12-10 21:48:07 +0100

    2019 p10

--- 2019/src/main/java/com/msendyka/adventofcode/p10/P10.java
+++ 2019/src/main/java/com/msendyka/adventofcode/p10/P10.java
@@ -1,107 +1,150 @@
 package com.msendyka.adventofcode.p10;
 
-import static java.lang.Math.abs;
+import com.msendyka.adventofcode.Functions;
+import com.msendyka.adventofcode.Point;
+import io.vavr.collection.List;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Objects;
 
-import com.msendyka.adventofcode.Functions;
-
-import io.vavr.collection.List;
+import static java.lang.Math.abs;
 
 public class P10 {
 
+    private static final boolean DEBUG = false;
+
     public static void main(String[] args) {
-        List<List<String>> map = Functions.readInput("p10/test.txt")
+        List<List<String>> map = Functions.readInput("p10/input.txt")
                 .map(line -> line.chars().mapToObj(c -> (char) c).map(String::valueOf))
                 .map(stream -> List.ofAll(stream));
 
         java.util.List<Point> asteroids = new ArrayList<>();
-        int size = map.head().size();
+        int width = map.head().size();
+        int height = map.size();
         List<String> strings = map.flatMap(List::ofAll);
-        for (int i = 0; i < map.size(); i++) {
-            for (int j = 0; j < size; j++) {
-                String s = strings.get(i * size + j);
-                System.out.print(s + " ");
+        visualiseInitialInput(asteroids, width, height, strings);
+
+        Point base = partOne(List.ofAll(asteroids), width, height);
+        System.out.println(base);
+        partTwo(map, List.ofAll(asteroids), width, strings, base);
+    }
+
+    private static void visualiseInitialInput(java.util.List<Point> asteroids, int width, int height, List<String> strings) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                String s = strings.get(i * width + j);
+                soutSameLine(s + " ");
                 if (s.equals("#")) {
                     asteroids.add(new Point(j, i));
 
                 }
             }
-            System.out.println("");
-        }
-
-        Point base = partOne(map, asteroids);
-        System.out.println(base);
-//        SkyMap skyMap = new SkyMap(map.head().size(), map.size());
-//        List<Point> closest = skyMap.findClosest(base, List.ofAll(asteroids));
-//        System.out.println(closest);
-        SkyMap skyMap = new SkyMap(map.head().size(), map.size());
-        List<Point> check = skyMap.findVisible(base, List.ofAll(asteroids));
-        List<Point> toKill = List.ofAll(asteroids).remove(base).filter(check::contains);
-        List<PointToDestory> toKillOrdered = toKill.map(p -> new PointToDestory(p, base));
-        toKillOrdered = toKillOrdered.sorted(comparator);
-        System.out.println(toKill);
-        for (int i = 0; i < map.size(); i++) {
-            for (int j = 0; j < size; j++) {
-                String s = strings.get(i * size + j);
-                if (base.y == i && base.x == j) {
-                    System.out.print("X" + " ");
-                } else if (toKill.contains(new Point(j, i))) {
-                    System.out.print("O" + " ");
-                } else {
-                    System.out.print(s + " ");
-                }
-
-            }
-            System.out.println("");
-        }
-    }
-    private static Comparator<PointToDestory> comparator = new Comparator<PointToDestory>() {
-        @Override
-        public int compare(PointToDestory pointToDestory, PointToDestory t1) {
-            int x = Integer.valueOf(pointToDestory.relativeToBase.x).compareTo(Integer.valueOf(t1.relativeToBase.x));
-            if (x == 0) {
-                return Integer.valueOf(pointToDestory.relativeToBase.y).compareTo(Integer.valueOf(t1.relativeToBase.y));
-            }
-            return x;
-        }
-    };
-    private static class PointToDestory {
-        private Point real;
-        private Point relativeToBase;
-
-        public PointToDestory(Point real, Point base) {
-            this.real = real;
-            this.relativeToBase = new Point(real.x - base.x, real.y - base.y);
-        }
-
-        @Override
-        public String toString() {
-            return "PointToDestory{" +
-                    "real=" + real +
-                    ", relativeToBase=" + relativeToBase +
-                    '}';
+            sout("");
         }
     }
 
-    private static Point partOne(List<List<String>> map, java.util.List<Point> points) {
+    private static Point partOne(List<Point> points, int width, int height) {
         int max = 0;
         Point bestPoint = null;
         for (Point a : points) {
-            SkyMap skyMap = new SkyMap(map.head().size(), map.size());
+            SkyMap skyMap = new SkyMap(width, height);
             List<Point> check = skyMap.findVisible(a, List.ofAll(points));
             List<Point> visible = List.ofAll(points).remove(a).filter(check::contains);
 
             if (visible.size() > max) {
                 max = visible.size();
                 bestPoint = a;
-                System.out.println(visible);
+                sout(visible);
             }
         }
         System.out.println(max);
         return bestPoint;
+    }
+
+    private static void partTwo(List<List<String>> map, List<Point> asteroids, int width, List<String> strings, Point base) {
+        List<Point> toDestroy = List.ofAll(asteroids);
+        int i = 1;
+        while (toDestroy.size() > 1) {
+            List<Point> killed = fire(map, toDestroy, width, strings, base);
+            for (Point p : killed) {
+                if (i == 200) {
+                    System.out.println(i + " " + p);
+                }
+                sout(i++ + " " + p);
+            }
+            toDestroy = toDestroy.removeAll(killed);
+        }
+    }
+
+    private static List<Point> fire(List<List<String>> map, List<Point> asteroids, int size, List<String> strings, Point base) {
+        SkyMap skyMap = new SkyMap(map.head().size(), map.size());
+        List<Point> visibleFromBase = skyMap.findVisible(base, List.ofAll(asteroids));
+        List<Point> toKill = List.ofAll(asteroids).remove(base).filter(visibleFromBase::contains);
+        List<Point> toKillOrdered = toKill.filter(p -> p.x >= base.x && p.y < base.y).sorted(new Comp(base));
+        toKillOrdered = toKillOrdered.appendAll(toKill.filter(p -> p.x >= base.x && p.y >= base.y).sorted(new Comp(base)));
+        toKillOrdered = toKillOrdered.appendAll(toKill.filter(p -> p.x < base.x && p.y >= base.y).sorted(new Comp(base)));
+        toKillOrdered = toKillOrdered.appendAll(toKill.filter(p -> p.x < base.x && p.y < base.y).sorted(new Comp(base)));
+        sout(toKillOrdered);
+        visualiseLaserShot(map, size, strings, base, toKill, toKillOrdered);
+        return toKillOrdered;
+    }
+
+    private static void visualiseLaserShot(List<List<String>> map, int size, List<String> strings, Point base, List<Point> toKill, List<Point> toKillOrdered) {
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < size; j++) {
+                String s = strings.get(i * size + j);
+                if (base.y == i && base.x == j) {
+                    soutSameLine("X" + " ");
+                } else if (toKill.contains(new Point(j, i))) {
+                    int order = toKillOrdered.indexOf(new Point(j, i));
+                    if (order == -1) order = 0;
+                    soutSameLine(order + " ");
+                } else {
+                    soutSameLine(s + " ");
+                }
+
+            }
+            sout("");
+        }
+    }
+
+    private static void sout(Object toPrint) {
+        if (DEBUG) {
+            System.out.println(toPrint);
+        }
+    }
+
+    private static void soutSameLine(Object toPrint) {
+        if (DEBUG) {
+            System.out.print(toPrint);
+        }
+    }
+    private static class Comp implements Comparator<Point> {
+
+
+        private Point basePoint;
+
+        public Comp(Point basePoint) {
+            this.basePoint = basePoint;
+        }
+
+        @Override
+        public int compare(Point o1, Point o2) {
+            Double o1Slope = slope(o1, basePoint);
+            Double o2Slope = slope(o2, basePoint);
+            return o1Slope.compareTo(o2Slope);
+        }
+        private Double slope(Point o1, Point o2) {
+            double divider = o2.x - o1.x;
+            if (divider == 0) {
+                return -100000.;
+            }
+            if (o2.y - o1.y == 0) {
+                return Double.MIN_VALUE;
+            }
+            return (o2.y - o1.y) / divider;
+        }
+
     }
 
     private static class SkyMap {
@@ -121,40 +164,11 @@ public class P10 {
             coordinates = List.ofAll(points);
         }
 
-        List<Point> findClosest(Point asteroid, List<Point> others) {
-            List<Point> result = List.ofAll(others);
-            for (Point b : others) {
-                List<Point> pointsBetween = List.of();
-                if (asteroid == b) {
-                    continue;
-                }
-                int dxc = b.x - asteroid.x;
-                int dyc = b.y - asteroid.y;
-                for (Point p : others) {
-                    if (p.equals(b)) {
-                        continue;
-                    }
-                    int dxl = p.x - asteroid.x;
-                    int dyl = p.y - asteroid.y;
-                    int cross = dxc * dyl - dyc * dxl;
-                    if (cross == 0 && between(dxl, dyl, asteroid, b, p)) {
-//                        System.out.println("\t\t" + p);
-                        result = result.remove(p);
-                    }
-                }
-//                result = result.removeAll(pointsBetween);
-                int x = 0;
-            }
-            return result;
-
-        }
 
         List<Point> findVisible(Point asteroid, List<Point> others) {
-//            System.out.println(asteroid);
             List<Point> result = List.ofAll(coordinates);
 
             for (Point b : others) {
-//                System.out.println("\t" + b);
                 if (asteroid == b) {
                     continue;
                 }
@@ -168,7 +182,6 @@ public class P10 {
                     int dyl = p.y - asteroid.y;
                     int cross = dxc * dyl - dyc * dxl;
                     if (cross == 0 && between(dxl, dyl, asteroid, b, p)) {
-//                        System.out.println("\t\t" + p);
                         result = result.remove(p);
                     }
                 }
@@ -191,39 +204,4 @@ public class P10 {
         }
     }
 
-    private static class Point {
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public String toString() {
-            return "Point{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Point point = (Point) o;
-            return x == point.x &&
-                    y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
 }
